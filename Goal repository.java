7d1a49df217942dbb8b1dea6f319d99417f70095
SUHETHA S <suhethas066@gmail.com>
2026-06-08 22:49:47 +0530

    Create GoalRepository interface for Goal entity

---
+++ Goal repository.java
@@ -0,0 +1,10 @@
+package com.dreamforge.repository;
+
+import com.dreamforge.model.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+
+}

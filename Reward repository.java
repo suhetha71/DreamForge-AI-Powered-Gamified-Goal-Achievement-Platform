21f48709d84323d6d9b8e8f33381f7e74ad32f8c
SUHETHA S <suhethas066@gmail.com>
2026-06-08 22:50:47 +0530

    Create RewardRepository interface for Reward model

---
+++ Reward repository.java
@@ -0,0 +1,10 @@
+package com.dreamforge.repository;
+
+import com.dreamforge.model.Reward;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RewardRepository extends JpaRepository<Reward, Long> {
+
+}

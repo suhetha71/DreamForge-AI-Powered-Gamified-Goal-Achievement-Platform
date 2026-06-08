d184f82b561a958aa50de0ee87fea24cd7272184
SUHETHA S <suhethas066@gmail.com>
2026-06-08 22:43:58 +0530

    Create User entity with fields and methods

---
+++ User.java
@@ -0,0 +1,95 @@
+package com.dreamforge.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    private int points;
+
+    private String badge;
+
+    public User() {
+    }
+
+    public User(String name, String email, String password, int points, String badge) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.points = points;
+        this.badge = badge;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+    public String getBadge() {
+        return badge;
+    }
+
+    public void setBadge(String badge) {
+        this.badge = badge;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", points=" + points +
+                ", badge='" + badge + '\'' +
+                '}';
+    }
+}
